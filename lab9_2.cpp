d1811a97c1c78e33cf7a88a275a7949c7ddc37ef
woramet2188 <76035801+woramet2188@users.noreply.github.com>
2021-01-08 14:53:19 +0700

    Initial commit

---
+++ lab9_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt") ;
+	dest.open("cheerbook_copy.txt");
+	
+	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+
+    source.close();
+    dest.close();
+	return 0;
+}
